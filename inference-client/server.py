589a491deea1ec72b11fb6838165f93645e0f35d
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2020-11-12 18:52:31 +0530

    added files for realtime inference service

---
+++ inference-client/server.py
@@ -0,0 +1,132 @@
+# set the project root directory as the static folder, you can set others.
+import grpc
+import threading
+import json
+from flask import Flask, request
+from flask import render_template
+
+from audio_to_text_pb2_grpc import RecognizeStub
+from audio_to_text_pb2 import Message
+from flask_socketio import SocketIO, emit
+from audio_grpc_client import RecognitionClient
+import time
+# creates a Flask application, named app
+app = Flask(__name__)
+socketio = SocketIO(app,cors_allowed_origins="*")
+client_buffers = {}
+client_transcriptions = {}
+
+def make_message(message,audio, id, mic_flag = "continue"):
+    return Message(
+        message=message,
+        audio=audio,
+        user=str(id),
+        mic_flag=mic_flag
+    )
+
+def _run_client(address, recognition_client):
+    with grpc.insecure_channel("localhost:55002") as channel:
+        print("connected to grpc")
+        stub = RecognizeStub(channel)
+        responses = stub.recognize_audio(recognition_client)
+        for resp in responses:
+            recognition_client.add_response(resp)
+
+
+@app.route("/")
+def index():
+        return render_template('index.html')
+
+def delete_globals(sid):
+    global client_buffers, client_transcriptions
+    if sid in client_buffers:
+        del client_buffers[sid]
+    if sid in client_transcriptions:
+        del client_transcriptions[sid]
+
+@socketio.on('disconnect')
+def test_disconnect():
+    delete_globals(request.sid)
+
+@socketio.on('end')
+def end_event(json):
+    #delete_globals(request.sid)
+    pass
+
+@socketio.on('start')
+def start_event(json):
+    delete_globals(request.sid)
+
+mic_ids = []
+
+@socketio.on('mic_data')
+def mic_data(chunk, speaking):
+    global client_buffers
+    sid = request.sid
+    mic_flag = "replace"
+    if(sid not in client_buffers):
+        client_buffers[sid] = chunk
+    else:
+        client_buffers[sid] = client_buffers[sid]+b''+chunk[100:]
+    buffer = client_buffers[sid]
+
+    if (not speaking):# or (len(client_buffers[sid]) >= 102400):
+        del client_buffers[sid]
+        mic_flag = "append"
+
+    msg = make_message("mic",buffer, sid, mic_flag)
+    print(sid, "send msg")
+    msg_id = str(int(time.time()*1000.0))+str(sid)
+    response = recognition_client_mic.recognize(msg, msg_id)
+    message = json.loads(response.message)
+    id = message["id"]
+    if id in mic_ids:
+        return
+    mic_ids.append(id)
+    is_success = message["success"]
+    if not is_success:
+        emit('unidentified', "Sentence not recognized by model", room=sid)
+        return
+    emit('unidentified', "", room=sid)
+    if sid not in client_transcriptions: client_transcriptions[sid] = ""
+    print(sid, message["transcription"])
+    transcription = client_transcriptions[sid] +" "+message["transcription"]
+    if response.mic_flag == "append":
+        client_transcriptions[sid] = transcription      
+      
+    emit('response', transcription, room=sid)
+
+id_dict = []
+
+@socketio.on('file_data')
+def file_data(chunk):
+    global id_dict
+    sid = request.sid
+    msg_id = str(int(time.time()*1000.0))+str(sid)
+    msg = make_message("file",chunk[100:], sid, "append")
+    print(sid, "received chunk")
+    response = recognition_client_file.recognize(msg, msg_id)
+    message = json.loads(response.message)
+    print(sid, "respons received")
+    id = message["id"]
+    is_success = message["success"]
+    if id in id_dict or not is_success:
+        print("id already present", is_success)
+        return
+    id_dict.append(id)
+    transcription = message["transcription"]
+    print(sid, "emitted res")
+    emit('file_upload_response', transcription, room=sid)
+
+
+if __name__ == "__main__":
+    global recognition_client_mic, recognition_client_file
+    recognition_client_mic = RecognitionClient()
+    recognition_client_file = RecognitionClient()
+    mic_client_thread = threading.Thread(target=_run_client, args=('localhost:50052', recognition_client_mic))
+    mic_client_thread.start()
+    file_client_thread = threading.Thread(target=_run_client, args=('localhost:50052', recognition_client_file))
+    file_client_thread.start()
+    socketio.run(app, host='0.0.0.0', port=9009)
+        
+
---
+++ wav2vec/realtime_inference_service.py
@@ -0,0 +1,56 @@
+import os
+import wave
+import grpc
+from concurrent import futures
+from audio_to_text_pb2_grpc import add_RecognizeServicer_to_server, RecognizeServicer
+from audio_to_text_pb2 import Response
+import time
+import json
+import io
+import json
+from inference_service import InferenceService
+
+class RecognizeAudioServicer(RecognizeServicer):
+    def __init__(self):
+        cwd = os.getcwd()
+        self.inference = InferenceService(cwd+"/final_custom_model.pt",cwd+"/dict.ltr.txt")
+        print(' Model Loaded Successfully')
+        self.count = 0
+
+    def recognize_audio(self, request_iterator, context):
+        for data in request_iterator:
+            self.count+=1
+            transcription, flag = self.transcribe(data.audio, data.user+str(self.count))
+            if(flag):
+                print(data.user, "responded")
+                yield Response(message=transcription, type=data.message,user=data.user, mic_flag=data.mic_flag)
+
+    def write_wave_to_file(self, file_name, audio):
+        with wave.open(file_name,'wb') as file:
+            file.setnchannels(1)
+            file.setsampwidth(2)
+            file.setframerate(16000.0)
+            file.writeframes(audio)
+        return os.path.join(os.getcwd(),file_name)
+
+    def transcribe(self, audio, index):
+        name = "wave_0%s.wav" % index
+        file_name = self.write_wave_to_file(name, audio)
+        result = self.inference.get_inference(file_name)
+        result["id"] = index
+        os.remove(file_name)
+        if(result['status'] != "OK"):
+            result["success"] = False
+        return json.dumps(result), True
+
+def serve():
+    PORT = 55002
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    add_RecognizeServicer_to_server(RecognizeAudioServicer(),server)
+    server.add_insecure_port('[::]:%d'%PORT)
+    server.start()
+    print("GRPC Server! Listening in port %d"%PORT)
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
