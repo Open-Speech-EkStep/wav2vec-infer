29ba37055225e77faec733fc20f30bfb84f73495
NireshKumarRaj <nireshkumar.r@thoughtworks.com>
2020-11-26 09:27:48 +0000

    Sreejith| stop mic when language change

--- inference-client/server.py
+++ inference-client/server.py
@@ -106,8 +106,9 @@ def mic_data(chunk, speaking, language):
     if response.mic_flag == "append":
         client_transcriptions[response.user] = transcription      
       
-    emit('response', {"transcription":transcription, "language":response.language}, room=response.user)
-
+    #emit('response', {"language": response.language, "transcription":transcription}, room=sid)
+    emit('response', transcription, room=sid)
+    
 id_dict = []
 
 @socketio.on('file_data')
@@ -134,11 +135,11 @@ def file_data(chunk, language):
 if __name__ == "__main__":
     global recognition_client_mic, recognition_client_file
     recognition_client_mic = RecognitionClient()
-    recognition_client_file = RecognitionClient()
+   #recognition_client_file = RecognitionClient()
     mic_client_thread = threading.Thread(target=_run_client, args=('localhost:55102', recognition_client_mic))
     mic_client_thread.start()
-    file_client_thread = threading.Thread(target=_run_client, args=('localhost:55102', recognition_client_file))
-    file_client_thread.start()
+   #file_client_thread = threading.Thread(target=_run_client, args=('localhost:55102', recognition_client_file))
+   #file_client_thread.start()
     socketio.run(app, host='0.0.0.0', port=9008)
         
 
