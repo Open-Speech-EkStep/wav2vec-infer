987f0516c26c423ef8db6f37c4d1a0e2ec795e6a
harveen <harveen>
2020-12-14 07:51:25 +0000

    Changes for Language Model

--- wav2vec/server.py
+++ wav2vec/server.py
@@ -11,6 +11,8 @@ from fairseq.models.wav2vec.wav2vec2_asr import base_architecture, Wav2VecEncode
 from wav2letter.common import create_word_dict, load_words
 from wav2letter.decoder import CriterionType,DecoderOptions,KenLM,LM,LMState,SmearingMode,Trie,LexiconDecoder
 from wav2letter.criterion import CpuViterbiPath, get_data_ptr_as_bytes
+import numpy as np
+from tqdm import tqdm
 
 import os
 from tempfile import NamedTemporaryFile
@@ -131,6 +133,96 @@ class W2lViterbiDecoder(W2lDecoder):
 
 
 
+class W2lKenLMDecoder(W2lDecoder):
+    def __init__(self, args, tgt_dict):
+        super().__init__( tgt_dict)
+
+        self.silence = (
+            tgt_dict.index("<ctc_blank>")
+            if "<ctc_blank>" in tgt_dict.indices
+            else tgt_dict.bos()
+        )
+        self.lexicon = load_words(args['lexicon'])
+        self.word_dict = create_word_dict(self.lexicon)
+        self.unk_word = self.word_dict.get_index("<unk>")
+
+        self.lm = KenLM(args['kenlm_model'], self.word_dict)
+        self.trie = Trie(self.vocab_size, self.silence)
+        print('h1')
+        print(len(self.lexicon.items()))
+        start_state = self.lm.start(False)
+        for i, (word, spellings) in enumerate(self.lexicon.items()):
+            print(i, word, spellings)
+            word_idx = self.word_dict.get_index(word)
+            _, score = self.lm.score(start_state, word_idx)
+            for spelling in spellings:
+                spelling_idxs = [tgt_dict.index(token) for token in spelling]
+                assert (
+                    tgt_dict.unk() not in spelling_idxs
+                ), f"{spelling} {spelling_idxs}"
+                self.trie.insert(spelling_idxs, word_idx, score)
+        self.trie.smear(SmearingMode.MAX)
+        print('h2')
+
+        if args['beam_size_token']:
+            argument_2 = int(args['beam_size_token'])
+        else:
+            argument_2 = int(len(tgt_dict))
+
+        self.decoder_opts = DecoderOptions(
+            args['beam'],
+            argument_2,
+            args['beam_threshold'],
+            args['lm_weight'],
+            args['word_score'],
+            args['unk_weight'],
+            args['sil_weight'],
+            0,
+            False,
+            self.criterion_type,
+        )
+
+        if self.asg_transitions is None:
+            N = 768
+            # self.asg_transitions = torch.FloatTensor(N, N).zero_()
+            self.asg_transitions = []
+
+        self.decoder = LexiconDecoder(
+            self.decoder_opts,
+            self.trie,
+            self.lm,
+            self.silence,
+            self.blank,
+            self.unk_word,
+            self.asg_transitions,
+            False,
+        )
+
+    def decode(self, emissions):
+        B, T, N = emissions.size()
+        hypos = []
+        print('Decoding with Kenlm')
+        for b in range(B):
+            emissions_ptr = emissions.data_ptr() + 4 * b * emissions.stride(0)
+            results = self.decoder.decode(emissions_ptr, T, N)
+
+            nbest_results = results[: self.nbest]
+            hypos.append(
+                [
+                    {
+                        "tokens": self.get_tokens(result.tokens),
+                        "score": result.score,
+                        "words": [
+                            self.word_dict.get_entry(x) for x in result.words if x >= 0
+                        ],
+                    }
+                    for result in nbest_results
+                ]
+            )
+        return hypos
+
+
+
 def get_results(wav_path,target_dict_path,use_cuda=False,w2v_path=None,model=None):
     sample = dict()
     net_input = dict()
@@ -140,7 +232,7 @@ def get_results(wav_path,target_dict_path,use_cuda=False,w2v_path=None,model=Non
     
     model[0].eval()
 
-    generator = W2lViterbiDecoder(target_dict)
+    #generator = W2lViterbiDecoder(target_dict)
     net_input["source"] = feature.unsqueeze(0)
 
     padding_mask = torch.BoolTensor(net_input["source"].size(1)).fill_(False).unsqueeze(0)
@@ -256,15 +348,32 @@ if __name__ == "__main__":
     parser.add_argument('-m', '--model-path', type=str, required=True, help="Model path")
     parser.add_argument('-c', '--cuda',default=False, type=bool, help="CUDA path")
     args_local = parser.parse_args()
-    global model_dict, cuda
-    
+    global model_dict, cuda, generator
+
     with open(args_local.model_path) as f:
         model_dict = json.load(f)
 
-    
+    dict_path = '/home/harveen.chadha/deployed_models/hi/dict.ltr.txt'
+
+    args_lm = {}
+    args_lm['lexicon'] = '/home/harveen.chadha/github/lm/LM_v2/lexicon.lst'
+    args_lm['kenlm_model'] = '/home/harveen.chadha/github/lm/LM_v2/lm.binary'
+    args_lm['beam'] = 128
+    args_lm['beam_threshold'] = 25
+    args_lm['lm_weight'] = 0.4
+    args_lm['word_score'] = 0.3
+    args_lm['unk_weight'] = -np.inf
+    args_lm['sil_weight'] = 0
+
+    print(args_lm)
+    print('heere')
+    #print(args_lm.lexicon)
+    print('heere 2')
+    target_dict = Dictionary.load(dict_path)
+    generator = W2lKenLMDecoder(args_lm, target_dict)
 
     cuda = args_local.cuda
     print(cuda) 
     logging.info('Server initialised')
-    app.run(host='0.0.0.0', port=8000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8020, debug=True, use_reloader=False)
 
