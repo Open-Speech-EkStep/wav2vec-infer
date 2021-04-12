ba1242f3c7aacb29f30f78e2a0263d81562b9278
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2021-04-12 10:54:36 +0530

    Update wav2vec_setup.sh

--- wav2vec_setup.sh
+++ wav2vec_setup.sh
@@ -52,7 +52,7 @@ pip install -e .
 New Models are here:
 
 
-gsutil -m cp -r gs://ekstepspeechrecognition-dev/deployed_models .
+Please bring your model using [Custom model script](https://github.com/Open-Speech-EkStep/vakyansh-wav2vec2-experimentation/blob/main/scripts/inference/generate_custom_model.sh) here.
 #### Kindly update model paths in model_dict.json
 
 
